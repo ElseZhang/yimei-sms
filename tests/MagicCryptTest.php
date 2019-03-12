ee6a9671642f879ceca3cf5da3e84bce32bd1fb4
ElseZhang <mzhang173@gmail.com>
2019-03-12 03:26:49 +0000

    Apply fixes from StyleCI

--- tests/MagicCryptTest.php
+++ tests/MagicCryptTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the elsezhang/yimei-sms.
+ *
+ * (c) overtrue <mzhang173@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace ElseZhang\YiMeiSms\Tests;
 
 use ElseZhang\YiMeiSms\MagicCrypt;
